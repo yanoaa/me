74d30a19241202d9f5de0decab4f7a4ec3b31570
Harineko0 <harineko0927@gmail.com>
2024-04-20 09:22:41 +0900

    fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: [
-                {
-                    loader: "@svgr/webpack",
-                },
-            ],
-        });
-        return config;
-    },
     images: {
-        disableStaticImages: true, // importした画像の型定義設定を無効にする
         unoptimized: true
     },
     output: "export",

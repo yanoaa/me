53eee12e00557e946cdf084f3558be81febe8055
Harineko0 <harineko0927@gmail.com>
2024-04-20 09:06:20 +0900

    fix runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const openSans = Open_Sans({ subsets: ["latin"] });
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 const notoSerifJP = Noto_Serif_JP({ subsets: [ "latin"], weight: "400" });
 
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 export const metadata: Metadata = {
   title: {

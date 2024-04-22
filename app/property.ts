f8743cba6dcd6fdc911a9eab6d3acefb8cbfdd04
yanoaa <42725929+yanoaa@users.noreply.github.com>
2024-04-22 08:31:39 +0000

    update link

--- app/property.ts
+++ app/property.ts
@@ -1,4 +1,4 @@
-export const yourName = "You";
-export const twitterLink = "https://twitter.com/"
+export const yourName = "yano";
+export const twitterLink = "https://twitter.com/yanosan_mj"
 export const instagramLink = "https://instagram.com/"
 export const githubLink = "https://instagram.com/"
